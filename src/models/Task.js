b291cdc597cea8c789d0b36bae4f6337e6d8b258
washingtondeveloper <washingtonprogramador@gmail.com>
2019-11-11 08:06:07 -0300

    add timestamp in task

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,18 +1,23 @@
 import mongoose from "mongoose";
 
-const TaskSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
+const TaskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    done: {
+      type: Boolean,
+      default: false
+    }
   },
-  description: {
-    type: String,
-    required: true
-  },
-  done: {
-    type: Boolean,
-    default: false
+  {
+    timestamps: true
   }
-});
+);
 
 module.exports = mongoose.model("Task", TaskSchema);
